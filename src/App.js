700c6718050545291872f4492859b29a1eef3ead
kodeneko <ladysun.freedom@gmail.com>
2022-02-09 13:06:12 +0100

    [master] hooks included

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import IdCard from "./idcard";
 
 function App() {
+  const [user, setUser] = useState(undefined);
 
-  const user = {
-    avatar: 'man.png',
-    name: 'Perico',
-    surname: 'De los Palotes',
-    city: 'Torrevieja',
-    state: 'Alicante'
-  }
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+    .then((data) => data.json())
+    .then((json) => {
+      const user = json.results[0];
+      setUser({
+        avatar: user.picture.medium,
+        name: user.name.first,
+        surname: user.name.last,
+        city: user.location.city,
+        state: user.location.state,
+      });
+    });
+  }, [])
 
-  return (
+  return user ? (
     <div className="App">
       <IdCard user={user} />
     </div>
-  );
+  ) : <img src="spinner.gif" alt="loading info" />;
 }
 
 export default App;
--- src/idcard/idcard.jsx
+++ src/idcard/idcard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const IdCard = (props) => {
   const {user} = props;
   return (
