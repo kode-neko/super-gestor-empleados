4c6dc70a5c745f202fededc6ba1e940228b0deed
kodeneko <ladysun.freedom@gmail.com>
2022-02-10 11:49:58 +0100

    [master] añadido btn añadir empelado

--- src/mainbar/mainbar.jsx
+++ src/mainbar/mainbar.jsx
@@ -4,7 +4,12 @@ const MainBar = ({ total, contratado }) => {
   return (
     <div className={styles.mainBar}>
       <h1 className={styles.title}>Super Gestor Empleados</h1>
-      <div className={styles.counter}>contratados {contratado}/{total}</div>
+      <div className={styles.right}>
+        <div className={styles.counter}>
+          contratados {contratado}/{total}
+        </div>
+        <button className={styles.add}>Añadir empleado</button>
+      </div>
     </div>
   );
 };
